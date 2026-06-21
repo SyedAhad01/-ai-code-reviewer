e3aadda6a2ab5c2bb0aaa3a3c7b65aa888b838ec
SyedAhad01 <syedahad11110@gmail.com>
2026-06-21 11:39:59 +0530

    serve frontend static files

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,13 @@
 require('dotenv').config()
 const app = require('./src/app')
+const path = require('path')
+
+app.use(require('express').static(path.join(__dirname, '../Frontend/dist')))
+
+app.get('/{*path}', (req, res) => {
+    res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'))
+})
 
 app.listen(3000, () => {
     console.log('server is running on http://localhost:3000')
-    
 })
